a21e920d38493ee7fc4c7fcb1f5b2f8fc5509bd2
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-10-11 14:53:09 -0400

    handle processes as individual data points

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 from geopy.geocoders import Nominatim
+import plotly.graph_objects as go
+import plotly.express as px
 
 # load data
 df = pd.read_csv("data/eau_potable_production.csv")
@@ -11,22 +13,35 @@ df = df.rename(columns={
     "Nom de la municipalité": "municipality", 
     "Numéro de l'installation de production": "installation_code",
     "Nom de l'installation de production d'eau potable": "installation_name",
-    "Procédé de traitement": "processes"
+    "Procédé de traitement": "process"
 })
 
-# temporary
-df = df[:50]
+# TODO: delete after testing with smaller daf 
+df = df[:5]
 print(df)
 
 # initialize geolocator
 geolocator = Nominatim(user_agent="my-application")
 
 # get coordinates
-df["coordinates"] = df["municipality"].apply(geolocator.geocode)
-df["latitide"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
-df["longitude"] = df["coordinates"].apply(lambda x: x.longitude if x != None else None)
+# df["coordinates"] = df["municipality"].apply(geolocator.geocode)
+# df["latitide"] = df["coordinates"].apply(lambda x: x.latitude if x != None else None)
+# df["longitude"] = df["coordinates"].apply(lambda x: x.longitude if x != None else None)
 
-print(df)
+# TODO: delete this after check if we don't need to define new columns
+# df["chloration"] = df["process"].apply(lambda x: True if "chloration" in x else False)
+# df["filtration"] = df["process"].apply(lambda x: True if "filtration" in x else False)
+# df["ultraviolet"] = df["process"].apply(lambda x: True if "ultraviolet" in x else False)
+# df["ozonation"] = df["process"].apply(lambda x: True if "ozonation" in x else False)
+# df["charbon"] = df["process"].apply(lambda x: True if "charbon" in x else False)
 
-#location = geolocator.geocode("Abitibi-Témiscamingue")
-#print(location.address)
+# create new dataframe to handle processes as individual data points
+new_list = []
+for index, row in df.iterrows():
+    for p in row["process"].split("\n"):
+        new_row = row.tolist()
+        new_row.pop() # TODO: need to remove element at certain index after adding coordinates?
+        new_row.append(p)
+        new_list.append(new_row)
+new_df = pd.DataFrame(new_list, columns=list(df.columns))
+print(new_df)
