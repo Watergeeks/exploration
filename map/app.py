660905d9630bb06430d093551fe33c9b87d2419f
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-10-21 11:12:17 -0400

    update app

--- map/app.py
+++ map/app.py
@@ -1,13 +1,8 @@
-import time
-import pathlib
-import os
-
 import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import State, Input, Output
-import dash_daq as daq
 
 app = dash.Dash(
     __name__,
