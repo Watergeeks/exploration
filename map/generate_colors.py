2e9be8273c0751be7c1a4b57a261961fa80ae204
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-10-23 09:21:35 -0400

    update colors

--- map/generate_colors.py
+++ map/generate_colors.py
@@ -1,29 +1,6 @@
-import random
 import seaborn as sns
 
-# def generate_colors(n):
-#   rgb_values = []
-#   hex_values = []
-#   r = 254 #int(random.random() * 256)
-#   g = 192 #int(random.random() * 256)
-#   b = 54 #int(random.random() * 256)
-#   step = 256 / n
-#   for _ in range(n):
-#     r += step
-#     g += step
-#     b += step
-#     r = int(r) % 256
-#     g = int(g) % 256
-#     b = int(b) % 256
-#     r_hex = hex(r)[2:]
-#     g_hex = hex(g)[2:]
-#     b_hex = hex(b)[2:]
-#     hex_values.append('#' + r_hex + g_hex + b_hex)
-#     rgb_values.append((r,g,b))
-#   return hex_values
-
 def generate_colors(n):
-  rgb_values = sns.color_palette(None, n)
+  rgb_values = sns.color_palette("husl", n)
   hex_values = rgb_values.as_hex()
   return hex_values
-
