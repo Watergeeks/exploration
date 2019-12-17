747e51b28b7502965ca821dd89656c2f20b32b04
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-17 16:39:49 -0500

    update paths

--- apps/SEAO/scrape.py
+++ apps/SEAO/scrape.py
@@ -17,10 +17,11 @@ def main(ARGS, IDS, FIELDS):
 
     def open_browser():
         # set path to chrome driver
-        if platform.system() == 'Windows':
-            CHROME_PATH = os.path.join('driver', 'chromedriver.exe')
-        else:
-            CHROME_PATH = os.path.join('driver', 'chromedriver')
+        CHROME_PATH = os.path.join('driver', 'chromedriver')
+        # if platform.system() == 'Windows':
+        #     CHROME_PATH = os.path.join('driver', 'chromedriver.exe')
+        # else:
+        #     CHROME_PATH = os.path.join('driver', 'chromedriver')
         # open browser and set window size
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
