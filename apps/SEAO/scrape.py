99ae315fc420d81ad85856d6ec8d4f37f1955c36
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-17 16:25:34 -0500

    update code structure

--- apps/SEAO/scrape.py
+++ apps/SEAO/scrape.py
@@ -1,14 +1,6 @@
-import time
-import argparse
-import platform
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
-import pandas as pd
 from arguments import *
 
-
-def main(ARGS, IDS):
+def main(ARGS, IDS, FIELDS):
 
     def check_if_element_exists(id):
         try:
@@ -17,65 +9,72 @@ def main(ARGS, IDS):
             return False
         return True
 
-    def click_if_element_exists(id):
-        if check_if_element_exists(id):
-            browser.find_element_by_id(id).click()
-
     def get_links(links):
         results = browser.find_element_by_id(IDS['results_table']).find_elements_by_tag_name('tr')
         for row in results[1:]:
             links.append(row.find_elements_by_tag_name('td')[1].find_element_by_tag_name('a').get_attribute('href'))
-        print(len(links))
         return links
 
-    # set path to chrome driver
-    if platform.system() == 'Windows':
-        CHROME_PATH = 'driver/chromedriver.exe'
-    else:
-        CHROME_PATH = 'driver/chromedriver'
+    def open_browser():
+        # set path to chrome driver
+        if platform.system() == 'Windows':
+            CHROME_PATH = 'driver/chromedriver.exe'
+        else:
+            CHROME_PATH = 'driver/chromedriver'
+        # open browser and set window size
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        browser = webdriver.Chrome(CHROME_PATH)
+        browser.set_window_size(1366, 768)
+        return browser
 
-    # open browser and set window size
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    browser = webdriver.Chrome(CHROME_PATH)
-    browser.set_window_size(1366, 768)
+    def login():
+        # visit login page and log in to redirect to dashboard
+        browser.get('https://www.seao.ca/Login.aspx')
+        browser.find_element_by_id(IDS['login_username']).send_keys(ARGS.username)
+        browser.find_element_by_id(IDS['login_password']).send_keys(ARGS.password)
+        browser.find_element_by_id(IDS['login_button']).click()
+        # pause
+        time.sleep(1)
+        # agree to renew session if asked
+        if check_if_element_exists(IDS['login_session']):
+            browser.find_element_by_id(IDS['login_session']).click()
+        # pause
+        time.sleep(1)
 
-    # visit login page and log in to redirect to dashboard
-    browser.get('https://www.seao.ca/Login.aspx')
-    browser.find_element_by_id(IDS['login_username']).send_keys(ARGS.username)
-    browser.find_element_by_id(IDS['login_password']).send_keys(ARGS.password)
-    browser.find_element_by_id(IDS['login_button']).click()
-
-    # pause
-    time.sleep(1)
-
-    # agree to renew session if asked
-    click_if_element_exists(IDS['login_session'])
-
-    # pause
-    time.sleep(1)
-
-    # visit advanced search page
-    browser.find_element_by_id(IDS['search_link']).click()
-
-    # pause
-    time.sleep(1)
-
-    # insert search criteria 
-    browser.find_element_by_id(IDS['search_any']).send_keys(ARGS.searchany)
-    browser.find_element_by_id(IDS['search_all']).send_keys(ARGS.searchall)
-    browser.find_element_by_id(IDS['search_button']).click()
+    def search():
+        # visit advanced search page
+        browser.find_element_by_id(IDS['search_link']).click()
+        # pause
+        time.sleep(1)
+        # insert search criteria 
+        browser.find_element_by_id(IDS['search_any']).send_keys(ARGS.searchany)
+        browser.find_element_by_id(IDS['search_all']).send_keys(ARGS.searchall)
+        browser.find_element_by_id(IDS['search_button']).click()
+        # TODO: finish working out how to retrieve desired UNSPSC codes
+        # # toggle search box for UNSPSC codes 
+        # browser.find_element_by_id(IDS['search_class_codes_button1']).click()
+        # # pause
+        # time.sleep(1)
+        # # check initial list of UNSPSC codes
+        # browser.find_element_by_id(IDS['search_class_codes_button2']).click()
+        # # ...
+        # pause
+        time.sleep(1)
+        # increase number of listings per page and sort listings by category
+        Select(browser.find_element_by_id(IDS['results_sort'])).select_by_visible_text('Catégorie')
+        Select(browser.find_element_by_id(IDS['results_limit'])).select_by_visible_text('100')
+        browser.find_element_by_id(IDS['results_button']).click()
+        # pause
+        time.sleep(2)
     
-    # pause
-    time.sleep(1)
-
-    # increase number of listings per page and sort listings by category
-    Select(browser.find_element_by_id(IDS['results_sort'])).select_by_visible_text('Catégorie')
-    Select(browser.find_element_by_id(IDS['results_limit'])).select_by_visible_text('100')
-    browser.find_element_by_id(IDS['results_button']).click()
-
-    # pause
-    time.sleep(2)
+    print('\n>>> Opening browser...')
+    browser = open_browser()
+    print('\n>>> Logging in...')
+    login()
+    print('\n>>> Searching...')
+    search()
+    print('\n>>> Gathering links of listings...')
 
     # initialize dictionary to store listing data as lists for each field before merging as columns of data frame
     listing = {}
@@ -96,7 +95,7 @@ def main(ARGS, IDS):
 
     print('\nSTATUS: gathered links to ' + str(len(listing['link'])) + ' listings from each page of search results\n')
 
-    # TODO: temporarily reduce length of links list to 5 for test
+    # TODO: temporarily reduce length of links list to 2 for test
     listing['link'] = listing['link'][:2]
     for link in listing['link']:
         # pause
@@ -142,9 +141,6 @@ def main(ARGS, IDS):
     browser.quit()
 
 
-##############################################################################################################
-
-
 if __name__ == '__main__':
     # define user arguments
     ARGS = Arguments()
@@ -153,4 +149,4 @@ if __name__ == '__main__':
     # define fields to scrape from listings
     FIELDS = ARGS.fields
     # run main method
-    main(ARGS, IDS) 
+    main(ARGS, IDS, FIELDS) 
