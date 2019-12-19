babef0a205cef7475e3b03a6688b0823503f62cc
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-19 14:30:53 -0500

    record time of scraping UNSPSC

--- apps/SEAO/scrape_UNSPSC_codes.py
+++ apps/SEAO/scrape_UNSPSC_codes.py
@@ -59,18 +59,26 @@ def scrape_codes():
         cells_names = browser.find_elements_by_xpath('//table[@class="'+IDS['category_table']+'"]/tbody/tr/td[3]')
         list_names = [cell.find_element_by_tag_name('label').text for cell in cells_names]
         return list_codes, list_names
-    # collect codes for each segment, family, class and commodity
+    # start recording time taken to scrape all segments
+    print('\nRecording time to scrape each segment:')
+    start_time = time.time()
+    # collect codes for each segment, family, class and commodity 
     # TODO: remove [:2] when ready to do for all codes
     list_segment_codes, list_segment_names = collect(None)
     for i in range(len(list_segment_codes[:2])):
+        # refresh timer to record time to scrape next segment
+        prev_time = time.time()
+        # note segment code and name
         s_c = list_segment_codes[i]
         s_n = list_segment_names[i]
         list_family_codes, list_family_names = collect(s_c)
         for j in range(len(list_family_codes[:2])):
+            # note family code and name
             f_c = list_family_codes[j]
             f_n = list_family_names[j]
             list_class_codes, list_class_names = collect(f_c)
             for k in range(len(list_class_codes[:2])):
+                # note class code and name
                 c_c = list_class_codes[k]
                 c_n = list_class_names[k]
                 list_commodity_codes, list_commodity_names = collect(c_c)
@@ -82,8 +90,15 @@ def scrape_codes():
                 codes['class_name'].extend([c_n for i in range(len(list_commodity_names))])
                 codes['family_name'].extend([f_n for i in range(len(list_commodity_names))])
                 codes['segment_name'].extend([s_n for i in range(len(list_commodity_names))])
+        # calculate time taken to scrape one segment
+        delta_time = round(time.time() - prev_time, 4)
+        print(s_c + ' ' + s_n + ': ' + str(delta_time) + ' seconds')
+    # calculate time taken to scrape all segments
+    delta_time = round(time.time() - start_time, 4)
+    print('All UNSPSC options: ' + str(delta_time) + ' seconds')
     # convert stored lists into dataframe and save as csv
     df_codes = pd.DataFrame(data=codes)
+    print('\n Data frame for codes/names of all UNSPSC options')
     print(df_codes)
     df_codes.to_csv('result_UNSPSC_codes.csv')
     # pause
